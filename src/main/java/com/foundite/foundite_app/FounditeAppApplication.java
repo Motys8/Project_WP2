c41f3cd7d528009d45b0673742dc26f68433bcf4
Hugo Chi <hugomotys@gmail.com>
2026-04-21 19:13:50 +0300

    Initial Commit : Springboot base

---
+++ src/main/java/com/foundite/foundite_app/FounditeAppApplication.java
@@ -0,0 +1,13 @@
+package com.foundite.foundite_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FounditeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FounditeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foundite/foundite_app/FounditeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foundite.foundite_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FounditeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
