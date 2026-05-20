d89053ceaf6b4e812ee08f40aa1303c1f21f72e3
Hugo Chi <hugomotys@gmail.com>
2026-05-20 23:48:29 +0300

    Update structure

--- src/main/java/com/foundite/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foundite.repository;
-
-import com.foundite.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
